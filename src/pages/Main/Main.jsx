9e98e580caa647b8957be987fd1674afba30cf0e
Weronika <weronika@silversheet.com>
2019-11-30 16:00:37 -0800

    construction view

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -4,7 +4,11 @@ import BaseScreen from "../../components/BaseScreen/BaseScreen";
 const Main = () => (
   <BaseScreen
     classNames="main-screen"
-  />
+  >
+    <div className="main">
+      this website is under construction. Come back soon!
+    </div>
+  </BaseScreen>
 );
 
 export default Main;
