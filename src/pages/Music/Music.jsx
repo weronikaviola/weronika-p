a71d7f9e55e303bfba86b1d0493a620f0fb94ea7
Weronika <weronika@silversheet.com>
2019-12-31 15:48:22 -0800

    fix blur errors

--- src/pages/Music/Music.jsx
+++ src/pages/Music/Music.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import FontAwesome from "react-fontawesome";
 import {
   MUSIC_DESCRIPTION,
@@ -9,23 +9,15 @@ import BaseScreen from "../../components/BaseScreen/BaseScreen";
 import Carousel from "../../components/Carousel/Carousel";
 import Slideshow from "../../components/Slideshow/Slideshow";
 
-const BLUR_OFFSET = 100;
-
 const Music = () => {
-  const [blurred, setBlurred] = useState(false);
   const moveDown = () => {
     document.getElementById("screen-2").scrollIntoView({ behavior: "smooth" });
   };
-  const onScroll = (evt) => {
-    const offset = evt.target.scrollTop;
-    if (offset > BLUR_OFFSET) setBlurred(true);
-    if (offset < BLUR_OFFSET) setBlurred(false);
-  };
 
   return (
-    <BaseScreen onScroll={onScroll}>
-      <div className="music-screen" id="screen-1">
-        <div className={`music-parallax ${blurred && "blur"}`}>
+    <BaseScreen>
+      <div className="music-screen height-90" id="screen-1">
+        <div className="music-parallax">
           <FontAwesome
             name="chevron-down"
             size="5x"
