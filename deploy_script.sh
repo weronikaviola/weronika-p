0266a7d21ce3e85b56473ebef240697b8cee1e37
= <weronika.altowka@gmail.com>
2021-04-25 20:05:25 -0700

    deploy script update

--- deploy_script.sh
+++ deploy_script.sh
@@ -4,9 +4,8 @@ set -e;
 
 echo "Looking for uncommited changes..."
 
-trap 'echo -e "\033[0;31mCHANGED\033[0m"' ERR
+trap 'echo -e "\033[0;31m🛑You have uncommited changes!🛑\033[0m"' ERR
 git diff-index --quiet HEAD --
-trap - ERR
 echo -e "\033[0;32mUNCHANGED\033[0m"
 
 env=$1;
@@ -16,7 +15,7 @@ remote="heroku-staging";
 if [ $env == "prod" ]
 then
   prod=1;
-  environment="heroku-prod"
+  remote="heroku-prod"
 fi
 
 echo "Deploying app to $remote. 🚀🚀🚀";
